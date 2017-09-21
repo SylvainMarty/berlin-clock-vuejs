7c674e849c4522b2d5bcc5d610f3b2afaaec1076
Sylvain Marty <sylvain.marty@aol.fr>
2017-09-21 18:48:22 +0200

    Initialisation du projet VueJS

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <berlin-clock></berlin-clock>
+  </div>
+</template>
+
+<script>
+import BerlinClock from './components/BerlinClock'
+
+export default {
+  name: 'app',
+  components: {
+    BerlinClock
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
